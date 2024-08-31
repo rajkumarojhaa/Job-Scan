4cbf056caceb4d07fe4c20704a024c96d5f91990
Rajkumar Ojha <78962286+rajkumarojhaa@users.noreply.github.com>
2024-08-31 21:50:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://job-scan-frontend.vercel.app/',
+    origin:'https://job-scan-frontend.onrender.com/',
     credentials:true
 }
 
